9ce6d1db9d6548b10cc6a739713c4cc852a74ecd
Nishi <nishi85@outlook.com>
2018-05-05 16:02:25 -0400

    styling the header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixIns = require('postcss-mixins');
+mixIns = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-     .pipe(postcss([cssImport, mixIns, cssvars, nested, autoprefixer]))
+     .pipe(postcss([cssImport, mixIns, cssvars, nested, hexrgba, autoprefixer]))
      .on('error', function(errorInfo){
         console.log(errorInfo.toString()); 
         this.emit('end');
